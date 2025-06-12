0be723bae11ca548c54ecdcb457971f3137badfc
Aadisnipe <adev96954@gmail.com>
2025-06-12 20:56:16 +0530

    new

--- web-dev/front-end/javascript/js_9/index.js
+++ web-dev/front-end/javascript/js_9/index.js
@@ -13,12 +13,12 @@
 
 //callback is used when we want to maintain a specific order by executing function in our desired order
 
-let a=[1,2,3,4,5];
-a.forEach(display); // for each value of array it is passing display function with element and index
-function display(elemet,index){
-    a[index]=elemet*2;
-    console.log(a);
-}
+let a=[99,56,4,5];
+// a.forEach(display); // for each value of array it is passing display function with element and index
+// function display(elemet,index){
+//     a[index]=elemet*2;
+//     console.log(a);
+// }
 
 //output
 
@@ -27,3 +27,49 @@ function display(elemet,index){
 // [2, 4, 6, 4, 5]
 // [2, 4, 6, 8, 5]
 // [2, 4, 6, 8, 10]
+
+// // we can also use map() method which also iterate each value of array and return new array with modified values
+// function square(element){
+//     return element*element;
+// }
+// let b=a.map(square);
+// console.log(b);
+
+// .filter() makes array only for true values
+// let c=a.filter(even)
+// function even (element){
+//     return element%2==0;
+// }
+// console.log(c);
+
+//.reduce() method is used to reduce the array to single value by passing previous and next values
+//first previous values is 0
+// let d=a.reduce(add);
+// function add(previous,next){
+//     return previous+next;
+// }
+// console.log(d);
+
+//settimeout this stops time in millisecond and then exexcute that function 1000ms =1s
+
+// setTimeout(function(){
+//     console.log("hello");
+// },5000);
+
+// arrow function is used to write a function in one line
+// variable = (pass parameter) => {write your code}
+// const b=(name,age) =>{console.log("hello "+name)
+//                       console.log("your age is "+age)};
+// b("adam","99");
+
+//prectice callback
+goodbye(hello);
+function hello() {
+    console.log("hello");
+}
+
+function goodbye(callback) {
+    console.log("goodbye");
+    callback()
+}
+window.print();
