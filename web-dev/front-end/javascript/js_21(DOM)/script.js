162dfdeb96fc3448517e36f789b5de9f5cab4376
Aadisnipe <adev96954@gmail.com>
2025-07-03 01:36:18 +0530

    Add initial HTML structure and JavaScript functionality for DOM manipulation

---
+++ web-dev/front-end/javascript/js_21(DOM)/script.js
@@ -0,0 +1,5 @@
+console.log("hello");
+console.log(typeof(document));  //entire html and css can be accesed through js because they are object
+console.dir(document); // from this direoctorywe can change anything
+document.title="pop";
+document.body.style.backgroundColor="hsl(0,0% ,15%)";
